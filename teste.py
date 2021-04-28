ab1f3de3f5d366393457e516dc8974f68eba0f92
MatheusCastel <matranec@gmail>
2021-04-28 16:16:15 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,12 +1,14 @@
 print('\033[31m'+'Isto eh vermelho'+'\033[0;0m')
 
-    for i in baralho:
-        if '♠' in i:
-            print('{}. {}'.format(contador, i))
-        elif '♥' in i:
-            print('\033[31m'+'{}. {}'.format(contador, i)+'\033[0;0m')
-        elif '♦' in i:
-            print('{}. {}'.format(contador, i))
-        else:
-            print('{}. {}'.format(contador, i))
-        contador +=1
+print('teste pra ver se eu existo')
+
+for i in baralho:
+    if '♠' in i:
+        print('{}. {}'.format(contador, i))
+    elif '♥' in i:
+        print('\033[31m'+'{}. {}'.format(contador, i)+'\033[0;0m')
+    elif '♦' in i:
+        print('{}. {}'.format(contador, i))
+    else:
+        print('{}. {}'.format(contador, i))
+    contador +=1

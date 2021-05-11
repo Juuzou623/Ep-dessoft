14e716bd099f3637704265da4d2c8bd0273b4d2f
Juuzou623 <pedrohois@al.insper.edu.br>
2021-05-11 15:40:45 -0300

    Retirando o que não será usado

--- teste.py
+++ teste.py
@@ -1,14 +1,3 @@
 print('\033[31m'+'Isto eh vermelho'+'\033[0;0m')
 
 print('teste pra ver se eu existo')
-
-for i in baralho:
-    if '♠' in i:
-        print('{}. {}'.format(contador, i))
-    elif '♥' in i:
-        print('\033[31m'+'{}. {}'.format(contador, i)+'\033[0;0m')
-    elif '♦' in i:
-        print('{}. {}'.format(contador, i))
-    else:
-        print('{}. {}'.format(contador, i))
-    contador +=1
